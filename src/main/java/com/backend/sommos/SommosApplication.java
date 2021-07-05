f9315f3eb868c80b5da7f0f8e66eebc3e4490125
dongmin <dongm1050@naver.com>
2021-07-05 17:09:06 +0900

    기본 세팅

---
+++ src/main/java/com/backend/sommos/SommosApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sommos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SommosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SommosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/sommos/SommosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sommos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SommosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

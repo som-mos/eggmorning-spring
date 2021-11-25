076b318ca9fbfa863483793217f04e9d9ba68508
YangDongmin <dongm1050@naver.com>
2021-11-25 21:44:58 +0900

    매개변수 추가

--- src/main/java/com/backend/sommos/domain/hotel/contoroller/HotelController.java
+++ src/main/java/com/backend/sommos/domain/hotel/contoroller/HotelController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,9 +27,26 @@ public class HotelController {
         this.hotelService = hotelService;
     }
 
+//    @ResponseBody
+//    @GetMapping("ranking/top")
+//    public List<HotelDTO> getTopRatingHotel(){
+//        return hotelService.getTop3RatingHotel().stream().map(hotel-> HotelMapper.convertToHotelDTO(hotel)).collect(Collectors.toList());
+//    }
+
     @ResponseBody
-    @GetMapping("ranking/top")
-    public List<HotelDTO> getTopRatingHotel(){
+    @GetMapping("ranking/top/{number}")
+    public List<HotelDTO> getTopRatingHotel(@PathVariable("number") int number){
+
+        if(number == 1){
+
+        }
         return hotelService.getTop3RatingHotel().stream().map(hotel-> HotelMapper.convertToHotelDTO(hotel)).collect(Collectors.toList());
     }
 }
+
+
+
+
+
+
+
